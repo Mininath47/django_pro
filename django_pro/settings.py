71e4813d61109c239d9d5fd4ce6f86c0997a65a9
Mininath47 <meninathhonmane953@gmail.com>
2024-11-25 15:27:13 +0530

    hi

--- django_pro/settings.py
+++ django_pro/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_pro',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
